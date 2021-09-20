6acccf33fefcbce16a821aeb22eadc34b3642c37
KostyaA30 <65397995+KostyaA30@users.noreply.github.com>
2021-09-20 23:10:17 +0300

    Update TOBIN.C

--- TOBIN.C
+++ TOBIN.C
@@ -4,5 +4,18 @@
 
 void ToBin( ulong X )
 {
+  INT i, element, f = 0;
   
+  for (i = 64; i >= 0; i--)
+  {
+    element = n & (1 << i);
+    if (flag == 1)
+    {
+      printf("%i", element);
+      continue;
+    }
+    if (element == 1)
+      flag = 1;
+    printf("&i", element);
+  }
 }
